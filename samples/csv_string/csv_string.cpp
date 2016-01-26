86d600be15f8941d935de7dec65c85a8258fe9d6
wojtek <wojtekkulma@gmail.com>
2016-01-26 17:57:59 +0100

    company name, begin,end Dates

--- samples/csv_string/csv_string.cpp
+++ samples/csv_string/csv_string.cpp
@@ -26,19 +26,89 @@
 #include "quote.h"
 #include <string>
 
+class Date
+{
+public:
+    int day;
+    int month;
+    int year;
+
+    int getDay() {
+        return this->day;
+    }
+
+    void setDay(int day) {
+        this->day = day;
+    }
+    int getMonth() {
+        return this->month;
+    }
+
+    void setMonth(int month) {
+        this->month = month;
+    }
+
+    int getYear() {
+        return this->year;
+    }
+
+    void setYear(int year) {
+        this->year = year;
+    }
+
+
+
+
+
+
+
+};
+
 
 int main(int argc, const char * argv[]) {
     try {
-        std::string company = "";
+        std::string companyName = "";
+        Date begin;
+        Date end;
+
+        int beginDay;
+        int beginMonth;
+        int beginYear;
+
+        int endDay;
+        int endMonth;
+        int endYear;
+
 
         std::cout << "company?" << std::endl;
-        std::cin >> company;
-        // more QuoteType types are available in QuoteTypes.h
-        std::cout << quote::getLatestQuotesCsv(company, {quote::QuoteType::symbol, quote::QuoteType::name, quote::QuoteType::lastTradePriceOnly, quote::QuoteType::lastTradeDate, quote::QuoteType::lastTradeTime, quote::QuoteType::open, quote::QuoteType::dayLow, quote::QuoteType::dayHigh, quote::QuoteType::dayRange, quote::QuoteType::dividendShare, quote::QuoteType::peRatio}) << std::endl;
-        // QuoteType types can also be passed as template parameters
+        std::cin >> companyName;
+
+        std::cout << "Begin day?" << std::endl;
+        std::cin >> beginDay;
+        std::cout << "Begin month?" << std::endl;
+        std::cin >> beginMonth;
+        std::cout << "Begin year?" << std::endl;
+        std::cin >> beginYear;
+
+        std::cout << "End day?" << std::endl;
+        std::cin >> endDay;
+        std::cout << "End month?" << std::endl;
+        std::cin >> endMonth;
+        std::cout << "End year?" << std::endl;
+        std::cin >> endMonth;
+
+
+        begin.setDay(beginDay);
+        begin.setMonth(beginMonth);
+        begin.setYear(beginYear);
+
+        end.setDay(endDay);
+        end.setMonth(endMonth);
+        end.setYear(endYear);
+
+        std::cout << quote::getLatestQuotesCsv(companyName, {quote::QuoteType::symbol, quote::QuoteType::name, quote::QuoteType::lastTradePriceOnly, quote::QuoteType::lastTradeDate, quote::QuoteType::lastTradeTime, quote::QuoteType::open, quote::QuoteType::dayLow, quote::QuoteType::dayHigh, quote::QuoteType::dayRange, quote::QuoteType::dividendShare, quote::QuoteType::peRatio}) << std::endl;
         std::cout << quote::getLatestQuotesCsv<quote::QuoteType::symbol, quote::QuoteType::name, quote::QuoteType::lastTradePriceOnly, quote::QuoteType::lastTradeDate, quote::QuoteType::lastTradeTime, quote::QuoteType::open, quote::QuoteType::dayLow, quote::QuoteType::dayHigh, quote::QuoteType::dayRange, quote::QuoteType::dividendShare, quote::QuoteType::peRatio>("MSFT") << std::endl;
-        std::cout << quote::getHistoricalQuotesCsv(company, 2010, 4, 1, 2014, 4, 21, quote::RangeType::dividendsOnly) << std::endl;
-        // if data can not be retrieved by any of the functions, an exception is thrown
+        std::cout << quote::getHistoricalQuotesCsv(companyName, begin.getYear(), begin.getMonth(), begin.getDay(), end.getYear(), end.getMonth(), end.getDay(), quote::RangeType::dividendsOnly) << std::endl;
     } catch (const std::exception& exception) {
         std::cerr << "Error: " << exception.what() << std::endl;
         return 1;
