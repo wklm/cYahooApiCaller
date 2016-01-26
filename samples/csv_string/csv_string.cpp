abcd2f6067af5a9542c5e9e2605cf00f4df90245
wojtek <wojtekkulma@gmail.com>
2016-01-26 16:53:31 +0100

    choose company

--- samples/csv_string/csv_string.cpp
+++ samples/csv_string/csv_string.cpp
@@ -24,19 +24,20 @@
 #include <iostream>
 //#include <curl/curl.h>
 #include "quote.h"
+#include <string>
+
 
 int main(int argc, const char * argv[]) {
     try {
-        // manual curl initialization must be performed to use quote functions in a multi-threaded context, otherwise it can be ignored.
-        // check http://curl.haxx.se/libcurl/c/curl_easy_init.html
-        //curl_global_init(CURL_GLOBAL_ALL);
+        std::string company = "";
 
-        std::cout << "downloading..." << std::endl;
+        std::cout << "company?" << std::endl;
+        std::cin >> company;
         // more QuoteType types are available in QuoteTypes.h
-        std::cout << quote::getLatestQuotesCsv("petr4.sa", {quote::QuoteType::symbol, quote::QuoteType::name, quote::QuoteType::lastTradePriceOnly, quote::QuoteType::lastTradeDate, quote::QuoteType::lastTradeTime, quote::QuoteType::open, quote::QuoteType::dayLow, quote::QuoteType::dayHigh, quote::QuoteType::dayRange, quote::QuoteType::dividendShare, quote::QuoteType::peRatio}) << std::endl;
+        std::cout << quote::getLatestQuotesCsv(company, {quote::QuoteType::symbol, quote::QuoteType::name, quote::QuoteType::lastTradePriceOnly, quote::QuoteType::lastTradeDate, quote::QuoteType::lastTradeTime, quote::QuoteType::open, quote::QuoteType::dayLow, quote::QuoteType::dayHigh, quote::QuoteType::dayRange, quote::QuoteType::dividendShare, quote::QuoteType::peRatio}) << std::endl;
         // QuoteType types can also be passed as template parameters
         std::cout << quote::getLatestQuotesCsv<quote::QuoteType::symbol, quote::QuoteType::name, quote::QuoteType::lastTradePriceOnly, quote::QuoteType::lastTradeDate, quote::QuoteType::lastTradeTime, quote::QuoteType::open, quote::QuoteType::dayLow, quote::QuoteType::dayHigh, quote::QuoteType::dayRange, quote::QuoteType::dividendShare, quote::QuoteType::peRatio>("MSFT") << std::endl;
-        std::cout << quote::getHistoricalQuotesCsv("petr4.sa", 2010, 4, 1, 2014, 4, 21, quote::RangeType::dividendsOnly) << std::endl;
+        std::cout << quote::getHistoricalQuotesCsv(company, 2010, 4, 1, 2014, 4, 21, quote::RangeType::dividendsOnly) << std::endl;
         // if data can not be retrieved by any of the functions, an exception is thrown
     } catch (const std::exception& exception) {
         std::cerr << "Error: " << exception.what() << std::endl;
